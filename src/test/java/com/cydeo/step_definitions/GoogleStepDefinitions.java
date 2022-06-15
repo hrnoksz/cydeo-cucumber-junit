84561f2474fbb035596983151286925b88c9fa0f
hrnoksz <harun1973@yahoo.com>
2022-06-15 11:33:04 +0300

    Search functionality result title verification is done

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -1,11 +1,15 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.GoogleSearchPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class GoogleStepDefinitions {
+
+    GoogleSearchPage googleSearchPage = new GoogleSearchPage();
 
     @When("user is on Google search page")
     public void user_is_on_google_search_page() {
@@ -18,6 +22,20 @@ public class GoogleStepDefinitions {
         String actualTitle = Driver.getDriver().getTitle();
         Assert.assertEquals(actualTitle, expectedTitle);
         Driver.closeDriver();
+    }
+
+    @When("user types apple and clicks enter")
+    public void user_types_apple_and_clicks_enter() {
+        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
+    }
+    @Then("user sees apple in the google title")
+    public void user_sees_apple_in_the_google_title() {
+        String expectedTitle = "apple - Google'da Ara";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        //Junit assertion accepts first argument as expected, second argument as actual
+        Assert.assertEquals("Title is not as expected!",expectedTitle, actualTitle);
+        //Assert.assertTrue(actualTitle.equals(expectedTitle));
     }
 
 }
--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -1,5 +1,6 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
@@ -44,8 +45,10 @@ public class Hooks {
     //import from io.cucumber.java not from junit
     @After
     public void teardownScenario(){
-        System.out.println("===Closing browser using cucumber @After===");
-        System.out.println("===Scenario ended/ Take screenshot if afiled!");
+
+        Driver.closeDriver();
+        //System.out.println("===Closing browser using cucumber @After===");
+        //System.out.println("===Scenario ended/ Take screenshot if failed!");
     }
 
     @BeforeStep
